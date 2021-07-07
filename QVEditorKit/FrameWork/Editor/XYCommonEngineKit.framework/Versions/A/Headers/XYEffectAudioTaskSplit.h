d64a1899d5f145719816b1c558d770ea2444da01
Sunshine <cheng.xia@quvideo.com>
2021-07-07 11:47:14 +0800

    feat: 创建工程添加clip及比例 错误码输出

---
+++ QVEditorKit/FrameWork/Editor/XYCommonEngineKit.framework/Versions/A/Headers/XYEffectAudioTaskSplit.h
@@ -0,0 +1,16 @@
+//
+//  XYEffectAudioTaskSplit.h
+//  XYCommonEngineKit
+//
+//  Created by 夏澄 on 2021/6/30.
+//
+
+#import "XYBaseEffectAudioTask.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XYEffectAudioTaskSplit : XYBaseEffectAudioTask
+
+@end
+
+NS_ASSUME_NONNULL_END
